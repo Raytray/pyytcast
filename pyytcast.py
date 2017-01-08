3d896416ac057b4df1e35e9a9cfe77b484a1be65
Raymond Tang <raymond@tang.me>
2017-01-08 15:31:15 -0500

    add get_latest_entry function

--- pyytcast.py
+++ pyytcast.py
@@ -3,7 +3,13 @@ import feedparser
 import pprint
 
 
+YOUTUBE_RSS_URL = "https://www.youtube.com/feeds/videos.xml?channel_id="
+
+
 def get_feeds(config_name='feeds.conf'):
+    """Takes in a configuration name (defaults to feeds.conf),
+    and returns a dictionary of youtube ids found in that file."""
+
     config = configparser.ConfigParser()
     config.read(config_name)
 
@@ -15,12 +21,21 @@ def get_feeds(config_name='feeds.conf'):
 
 
 def get_latest_entry(feeds):
+    """Takes in a dictionary of youtube channel ids created by get_feeds,
+    returns a list of youtube urls from the latest entry"""
+    results = []
     for feed in feeds:
-        pass
+        parsed_feed = feedparser.parse("{}{}".format(YOUTUBE_RSS_URL,
+                                                     feeds[feed]['channel_id']))
+        if parsed_feed and len(parsed_feed.entries) > 0:
+            results.append(parsed_feed.entries[0].link)
+
+    return results
 
 
 def main():
     feeds = get_feeds()
+    videos = get_latest_entry(feeds)
 
 
 if __name__ == "__main__":
--- test_pyytcast.py
+++ test_pyytcast.py
@@ -1,5 +1,7 @@
 import pyytcast
+import mock
 import unittest
+
 
 
 class TestPyYtcast(unittest.TestCase):
@@ -10,3 +12,23 @@ class TestPyYtcast(unittest.TestCase):
                     'test test': {'channel_id': 'another_test_id'}}
 
         self.assertEqual(expected, test_result)
+
+
+    @mock.patch('feedparser.parse')
+    def test_get_latest_entry(self, mock_feedparser_call):
+        mocked_feed = Object()
+        entry = Object()
+        entry.link = 'asdf'
+        mocked_feed.entries = [entry]
+
+        mock_feedparser_call.return_value = mocked_feed
+
+        get_latest_entry_param = {'test': {'channel_id': 'id'}}
+        test_result = pyytcast.get_latest_entry(get_latest_entry_param)
+
+        self.assertEqual([mocked_feed.entries[0].link], test_result)
+
+
+class Object(object):
+    """Used to create arbitrary objects in test"""
+    pass
