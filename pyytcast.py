a587b7b60f231ca622cfe80efe43239ae10ee1f0
Raymond Tang <raymond@tang.me>
2017-01-09 20:16:10 -0500

    full run through, create feed

--- pyytcast.py
+++ pyytcast.py
@@ -1,42 +1,75 @@
 import configparser
 import feedparser
-import pprint
+import youtube_dl
+
+from feedgen.feed import FeedGenerator
 
 
-YOUTUBE_RSS_URL = "https://www.youtube.com/feeds/videos.xml?channel_id="
-
-
-def get_feeds(config_name='feeds.conf'):
+def get_channel_ids(config_name='config.conf'):
     """Takes in a configuration name (defaults to feeds.conf),
-    and returns a dictionary of youtube ids found in that file."""
-
+    and returns a list of youtube ids found in that file."""
     config = configparser.ConfigParser()
     config.read(config_name)
 
-    result = {}
-    for section in config.sections():
-        result[section] = {'channel_id': config[section]['channel_id']}
+    result = config.get('youtube', 'channel_ids')
+    result = [channel_id.strip() for channel_id in result.split(",")]
 
     return result
 
 
-def get_latest_entry(feeds):
-    """Takes in a dictionary of youtube channel ids created by get_feeds,
-    returns a list of youtube urls from the latest entry"""
-    results = []
-    for feed in feeds:
-        parsed_feed = feedparser.parse(
-            "{}{}".format(YOUTUBE_RSS_URL,
-                          feeds[feed]['channel_id']))
-        if parsed_feed and len(parsed_feed.entries) > 0:
-            results.append(parsed_feed.entries[0].link)
+def generate_feed(channel_id):
+    """Given a channel id, obtain the youtube rss feed.
+    Download any new entries, convert to audio.
+    Attach and publish a feed for it.
 
-    return results
+    Do any clean up as necessary."""
+    parsed_feed = feedparser.parse("{}{}".format(
+        "https://www.youtube.com/feeds/videos.xml?channel_id=", channel_id))
+
+    generated_feed = FeedGenerator()
+    generated_feed.title(parsed_feed.feed.title)
+    generated_feed.load_extension('podcast')
+    generated_feed.link(
+        href='https://files.raytray.io/{}.xml'.format(channel_id), rel='self')
+    generated_feed.link(
+        href='https://raytray.io/', rel='alternate')
+    generated_feed.description(parsed_feed.feed.title)
+
+    for entry in parsed_feed.entries[:5]:
+        download_new_entries(entry.link)
+        feed_entry = generated_feed.add_entry()
+        feed_entry.id(entry.yt_videoid)
+        feed_entry.title(entry.title)
+        feed_entry.description(entry.summary)
+        feed_entry.enclosure('downloads/{}.mp3'.format(entry.yt_videoid),
+                             0, 'audio/mpeg')
+
+        generated_feed.add_entry(feed_entry)
+
+    generated_feed.rss_str(pretty=True)
+    generated_feed.rss_file('{}.xml'.format(channel_id))
+
+    # TODO: Clean up old files
+
+
+def download_new_entries(link):
+    """Download first 5 entries if not yet downloaded."""
+    youtube_dl_options = {'format': 'bestaudio',
+                      'postprocessors': [{
+                          'key': 'FFmpegExtractAudio',
+                          'preferredcodec': 'mp3',
+                          'preferredquality': '192'
+                      }],
+                      'download_archive': 'downloaded-videos.log',
+                      'outtmpl': './downloads/%(id)s.%(ext)s'}
+    with youtube_dl.YoutubeDL(youtube_dl_options) as ydl:
+        ydl.download([link])
 
 
 def main():
-    feeds = get_feeds()
-    videos = get_latest_entry(feeds)
+    channel_ids = get_channel_ids()
+    for channel_id in channel_ids:
+        generate_feed(channel_id)
 
 
 if __name__ == "__main__":
--- test_pyytcast.py
+++ test_pyytcast.py
@@ -1,32 +1,10 @@
 import pyytcast
-import mock
 import unittest
 
 
 class TestPyYtcast(unittest.TestCase):
-    def test_get_feeds(self):
-        test_result = pyytcast.get_feeds(config_name='test_feeds.conf')
+    def test_get_channel_ids(self):
+        test_result = pyytcast.get_channel_ids(config_name='test_feeds.conf')
 
-        expected = {'test': {'channel_id': 'test_id'},
-                    'test test': {'channel_id': 'another_test_id'}}
-
+        expected = ['test_id', 'another_test_id', 'test_with_strip']
         self.assertEqual(expected, test_result)
-
-    @mock.patch('feedparser.parse')
-    def test_get_latest_entry(self, mock_feedparser_call):
-        mocked_feed = Object()
-        entry = Object()
-        entry.link = 'asdf'
-        mocked_feed.entries = [entry]
-
-        mock_feedparser_call.return_value = mocked_feed
-
-        get_latest_entry_param = {'test': {'channel_id': 'id'}}
-        test_result = pyytcast.get_latest_entry(get_latest_entry_param)
-
-        self.assertEqual([mocked_feed.entries[0].link], test_result)
-
-
-class Object(object):
-    """Used to create arbitrary objects in test"""
-    pass
