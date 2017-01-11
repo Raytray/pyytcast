00f5acdeaa2837896f404a11818ed1575ff35684
Raymond Tang <raymond@tang.me>
2017-01-11 18:13:29 -0500

    quick dirty solution to cleaning up podcasts

---
+++ cleanup_podcasts.py
@@ -0,0 +1,23 @@
+import os
+
+
+def main():
+    mypath = "/var/www/files"
+    downloads_folder = "downloads"
+    files = [f for f in os.listdir("{}/{}".format(mypath, downloads_folder))
+             if f.endswith(".mp3")]
+
+    feeds = [f for f in os.listdir(mypath) if f.endswith(".xml")]
+
+    to_delete = files
+    for feed_name in feeds:
+        for file_name in files:
+            if file_name in open("{}/{}".format(mypath, feed_name)).read():
+                to_delete.remove(file_name)
+
+
+    for file_name in to_delete:
+        os.remove("{}/{}/{}".format(mypath, downloads_folder, file_name))
+
+if __name__ == "__main__":
+    main()
