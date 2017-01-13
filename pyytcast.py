bf41bdfcb09df789fe07324251adfa83ef13ce61
Raymond Tang <raymond@tang.me>
2017-01-12 19:48:58 -0500

    globalize configs

--- pyytcast.py
+++ pyytcast.py
@@ -8,6 +8,10 @@ from feedgen.feed import FeedGenerator
 config = configparser.ConfigParser()
 config.read('config.conf')
 
+MP3_FOLDER = config.get('paths', 'mp3_folder')
+URL = config.get('paths', 'URL')
+DEPLOY_PATH = config.get('paths', 'DEPLOY_PATH')
+
 
 def get_channel_ids():
     """Takes in a configuration name (defaults to feeds.conf),
@@ -25,11 +29,9 @@ def generate_feed(channel_id):
     parsed_feed = feedparser.parse('{}{}'.format(
         'https://www.youtube.com/feeds/videos.xml?channel_id=', channel_id))
 
-    url = config.get('paths', 'url')
-    generated_feed = setup_feed(channel_id, parsed_feed.feed.title, url)
+    generated_feed = setup_feed(channel_id, parsed_feed.feed.title)
 
     files_to_keep = []
-    mp3_folder = config.get('paths', 'mp3_folder')
     for entry in parsed_feed.entries[:5]:
         download_new_entries(entry.link)
 
@@ -39,7 +41,7 @@ def generate_feed(channel_id):
         feed_entry.description(entry.summary)
 
         feed_entry.enclosure('{}{}/{}.mp3'.format(
-            url, mp3_folder, entry.yt_videoid), 0, 'audio/mpeg')
+            URL, MP3_FOLDER, entry.yt_videoid), 0, 'audio/mpeg')
         generated_feed.add_entry(feed_entry)
 
         files_to_keep.append('{}.mp3'.format(entry.yt_videoid))
@@ -51,13 +53,13 @@ def generate_feed(channel_id):
     return files_to_keep
 
 
-def setup_feed(channel_id, title, url):
+def setup_feed(channel_id, title):
     """Create the feed with initial information and return it"""
     generated_feed = FeedGenerator()
     generated_feed.title(title)
     generated_feed.load_extension('podcast')
     generated_feed.link(
-        href='{}{}.xml'.format(url, channel_id), rel='self')
+        href='{}{}.xml'.format(URL, channel_id), rel='self')
     generated_feed.link(
         href='https://raytray.io/', rel='alternate')
     generated_feed.description(title)
@@ -75,30 +77,29 @@ def download_new_entries(link):
                               'preferredquality': '192'
                           }],
                           'download_archive': 'downloaded-videos.log',
-                          'outtmpl': './downloads/%(id)s.%(ext)s'}
+                          'outtmpl': '{}/{}/%(id)s.%(ext)s'.format(
+                              DEPLOY_PATH, MP3_FOLDER)}
     with youtube_dl.YoutubeDL(youtube_dl_options) as ydl:
         ydl.download([link])
 
 
 def cleanup_old_files(files_to_keep):
-    """Delete files matching mp3 in the mp3_folder that are not found
+    """Delete files matching mp3 in the MP3_FOLDER that are not found
     in files_to_keep.
-    Delete files matching .xml in deploy_path not found in files_to_keep"""
-    deploy_path = config.get('paths', 'deploy_path')
-    mp3_folder = config.get('paths', 'mp3_folder')
-    mp3s = [mp3 for mp3 in os.listdir('{}/{}'.format(deploy_path, mp3_folder))
+    Delete files matching .xml in DEPLOY_PATH not found in files_to_keep"""
+    mp3s = [mp3 for mp3 in os.listdir('{}/{}'.format(DEPLOY_PATH, MP3_FOLDER))
             if mp3.endswith(".mp3")]
 
-    feeds = [feed for feed in os.listdir(deploy_path) if feed.endswith('.xml')]
+    feeds = [feed for feed in os.listdir(DEPLOY_PATH) if feed.endswith('.xml')]
 
-    to_delete = ["{}/{}".format(mp3_folder, old_file) for old_file in mp3s
+    to_delete = ["{}/{}".format(MP3_FOLDER, old_file) for old_file in mp3s
                  if old_file not in files_to_keep]
     to_delete.extend([old_file for old_file in feeds
                       if old_file not in files_to_keep])
 
     for file_name in to_delete:
         print("Removing {}".format(file_name))
-        os.remove("{}/{}".format(deploy_path, file_name))
+        os.remove("{}/{}".format(DEPLOY_PATH, file_name))
 
 
 def main():
