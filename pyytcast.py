bea73d4d6ed7aa99d342a8517e8c5c01af0b151f
Raymond Tang <raymond@tang.me>
2017-01-12 19:32:16 -0500

    integrate cleanup files into pyytcast
    * move paths into config
    * add test for cleanup file

--- pyytcast.py
+++ pyytcast.py
@@ -1,18 +1,19 @@
 import configparser
 import feedparser
+import os
 import youtube_dl
 
 from feedgen.feed import FeedGenerator
 
+config = configparser.ConfigParser()
+config.read('config.conf')
 
-def get_channel_ids(config_name='config.conf'):
+
+def get_channel_ids():
     """Takes in a configuration name (defaults to feeds.conf),
     and returns a list of youtube ids found in that file."""
-    config = configparser.ConfigParser()
-    config.read(config_name)
-
     result = config.get('youtube', 'channel_ids')
-    result = [channel_id.strip() for channel_id in result.split(",")]
+    result = [channel_id.strip() for channel_id in result.split(',')]
 
     return result
 
@@ -20,37 +21,49 @@ def get_channel_ids(config_name='config.conf'):
 def generate_feed(channel_id):
     """Given a channel id, obtain the youtube rss feed.
     Download any new entries, convert to audio.
-    Attach and publish a feed for it.
+    Attach and publish a feed for it."""
+    parsed_feed = feedparser.parse('{}{}'.format(
+        'https://www.youtube.com/feeds/videos.xml?channel_id=', channel_id))
 
-    Do any clean up as necessary."""
-    parsed_feed = feedparser.parse("{}{}".format(
-        "https://www.youtube.com/feeds/videos.xml?channel_id=", channel_id))
+    url = config.get('paths', 'url')
+    generated_feed = setup_feed(channel_id, parsed_feed.feed.title, url)
 
-    generated_feed = FeedGenerator()
-    generated_feed.title(parsed_feed.feed.title)
-    generated_feed.load_extension('podcast')
-    generated_feed.link(
-        href='https://files.raytray.io/{}.xml'.format(channel_id), rel='self')
-    generated_feed.link(
-        href='https://raytray.io/', rel='alternate')
-    generated_feed.description(parsed_feed.feed.title)
-
+    files_to_keep = []
+    mp3_folder = config.get('paths', 'mp3_folder')
     for entry in parsed_feed.entries[:5]:
         download_new_entries(entry.link)
+
         feed_entry = generated_feed.add_entry()
         feed_entry.id(entry.yt_videoid)
         feed_entry.title(entry.title)
         feed_entry.description(entry.summary)
-        feed_entry.enclosure(
-            'https://files.raytray.io/downloads/{}.mp3'.format(
-                entry.yt_videoid), 0, 'audio/mpeg')
 
+        feed_entry.enclosure('{}{}/{}.mp3'.format(
+            url, mp3_folder, entry.yt_videoid), 0, 'audio/mpeg')
         generated_feed.add_entry(feed_entry)
 
-    generated_feed.rss_str(pretty=True)
-    generated_feed.rss_file('{}.xml'.format(channel_id))
+        files_to_keep.append('{}.mp3'.format(entry.yt_videoid))
 
-    # TODO: Clean up old files
+    feed_name = '{}.xml'.format(channel_id)
+    generated_feed.rss_file(feed_name)
+    files_to_keep.append(feed_name)
+
+    return files_to_keep
+
+
+def setup_feed(channel_id, title, url):
+    """Create the feed with initial information and return it"""
+    generated_feed = FeedGenerator()
+    generated_feed.title(title)
+    generated_feed.load_extension('podcast')
+    generated_feed.link(
+        href='{}{}.xml'.format(url, channel_id), rel='self')
+    generated_feed.link(
+        href='https://raytray.io/', rel='alternate')
+    generated_feed.description(title)
+    generated_feed.rss_str(pretty=True)
+
+    return generated_feed
 
 
 def download_new_entries(link):
@@ -67,11 +80,37 @@ def download_new_entries(link):
         ydl.download([link])
 
 
+def cleanup_old_files(files_to_keep):
+    """Delete files matching mp3 in the mp3_folder that are not found
+    in files_to_keep.
+    Delete files matching .xml in deploy_path not found in files_to_keep"""
+    deploy_path = config.get('paths', 'deploy_path')
+    mp3_folder = config.get('paths', 'mp3_folder')
+    mp3s = [mp3 for mp3 in os.listdir('{}/{}'.format(deploy_path, mp3_folder))
+            if mp3.endswith(".mp3")]
+
+    feeds = [feed for feed in os.listdir(deploy_path) if feed.endswith('.xml')]
+
+    to_delete = ["{}/{}".format(mp3_folder, old_file) for old_file in mp3s
+                 if old_file not in files_to_keep]
+    to_delete.extend([old_file for old_file in feeds
+                      if old_file not in files_to_keep])
+
+    for file_name in to_delete:
+        print("Removing {}".format(file_name))
+        os.remove("{}/{}".format(deploy_path, file_name))
+
+
 def main():
     channel_ids = get_channel_ids()
+    files_to_keep = []
     for channel_id in channel_ids:
-        generate_feed(channel_id)
+        files_to_keep.extend(generate_feed(channel_id))
+
+    cleanup_old_files(files_to_keep)
 
 
-if __name__ == "__main__":
+
+
+if __name__ == '__main__':
     main()
--- test_pyytcast.py
+++ test_pyytcast.py
@@ -1,10 +1,24 @@
 import pyytcast
+import mock
 import unittest
 
 
 class TestPyYtcast(unittest.TestCase):
     def test_get_channel_ids(self):
-        test_result = pyytcast.get_channel_ids(config_name='test_feeds.conf')
+        test_result = pyytcast.get_channel_ids()
 
         expected = ['test_id', 'another_test_id', 'test_with_strip']
         self.assertEqual(expected, test_result)
+
+
+    @mock.patch('os.listdir')
+    @mock.patch('os.remove')
+    def test_cleanup_old_files(self, mock_os_listdir_call, mock_remove_call):
+        mocked_directory = ['test.mp3', 'test2.mp3', 'test3.mp3']
+        mock_os_listdir_call.return_value = mocked_directory
+
+        test_params = mocked_directory
+        test_params.pop(0) # remove first element
+        pyytcast.cleanup_old_files(test_params)
+
+        self.assertTrue(mock_remove_call.called_once_with(mocked_directory[0]))
